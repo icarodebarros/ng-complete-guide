e2e8fa250c4b009ad35763455aea8627528a53c0
Icaro de Barros <icarodebarros@gmail.com>
2020-12-29 15:58:54 -0300

    Conteudo basico dos componentes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  public recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simple test', 'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_1280.jpg')
+  ];
 
   constructor() { }
 
