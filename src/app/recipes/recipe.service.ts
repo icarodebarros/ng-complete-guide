52cd59badfa9506307216bb3276bb3a79989359e
Icaro de Barros <icarodebarros@gmail.com>
2022-01-11 17:41:36 -0300

    Adding new functions to recipeServices

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shoppint-list.service";
@@ -6,6 +7,7 @@ import { Recipe } from "./recipe.model";
 
 @Injectable() // Necessário apenas quando se quer injetar algo nessa classe
 export class RecipeService {
+    recipesChanged = new Subject<Recipe[]>();
 
     private recipes: Recipe[] = [
         new Recipe(
@@ -39,6 +41,21 @@ export class RecipeService {
 
     getRecipe(index: number): Recipe {
         return this.recipes[index];
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.getRecipes());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.getRecipes());
+    }
+
+    deleteRecipe(index: number) {
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.getRecipes());
     }
 
 }
